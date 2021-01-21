6d0c493aa5a1688907523c5e180e79ba7660a7c7
Tatsuya Yamanoi <71238768+TatsuyaYamanoi-sys@users.noreply.github.com>
2021-01-21 22:21:28 +0900

    色々修正しました。。（もっと細かくバックアップすべきでした。。）

---
+++ script/main.js
@@ -0,0 +1,19 @@
+$(function() {
+    const main = new Main();
+});
+
+class Main {
+    constructor() {
+        this._init();
+    }
+    _init() {
+        this._bgParallax();
+    }
+    _bgParallax() {
+        $(window).on('scroll load', function(){
+            var bg = $(window).scrollTop();
+            $('.bg01').css({top:bg/10*-1});
+            $('.bg02').css({top:bg/5*-1});
+        });
+    }
+}
